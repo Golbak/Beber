6c62d9d43a37550748a3439add5ad8f01501babb
Golbak <r.zordek@hotmail.com>
2015-01-08 17:12:44 +0100

    V 0.0.2
    Cambiado index.jade e routers.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var juegos=[	{"juego":"El círculo de la muerte" },
+				{"juego":"Mayor o menor"},
+				{"juego":"Piramide"},
+				{"juego":"Autobús"},
+				{"juego":"Escalera"} ];
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' ,juegos : juegos});
 });
 
 module.exports = router;
